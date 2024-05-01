0231df2c4fcada702e947c014a0f3733a0ed53c4
Иван <jazzman80@yandex.ru>
2024-05-01 17:25:33 +0300

    NavHostFragment, NavGraph

--- app/src/main/java/ru/craftapps/repofinder/core/MainActivity.kt
+++ app/src/main/java/ru/craftapps/repofinder/core/MainActivity.kt
@@ -1,6 +1,14 @@
 package ru.craftapps.repofinder.core
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ru.craftapps.repofinder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
